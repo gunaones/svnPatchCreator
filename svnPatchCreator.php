022fd14c1bd6aba93625a36cb78210fedeaaf982
gunaones <prestasiku@gmail.com>
2015-10-18 01:25:52 +0700

    merapihkan

--- svnPatchCreator.php
+++ svnPatchCreator.php
@@ -68,15 +68,15 @@ class svnPatchCreator {
 }
 
 function arguments($argv) {
-    $_ARG = array();
-    foreach ($argv as $arg) {
-      if (ereg('--([^=]+)=(.*)',$arg,$reg)) {
-        $_ARG[$reg[1]] = $reg[2];
-      } elseif(ereg('-([a-zA-Z0-9])',$arg,$reg)) {
-            $_ARG[$reg[1]] = 'true';
-        }
-    }
-  return $_ARG;
+	$_ARG = array();
+	foreach ($argv as $arg) {
+		if (ereg('--([^=]+)=(.*)',$arg,$reg)) {
+			$_ARG[$reg[1]] = $reg[2];
+		} elseif(ereg('-([a-zA-Z0-9])',$arg,$reg)) {
+			$_ARG[$reg[1]] = 'true';
+		}
+	}
+	return $_ARG;
 }
 
 function read ($length='255') {
